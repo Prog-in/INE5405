2b60602058fba6f7f2fa513937c96753d57d9436
João Gabriel Feres <joaogabrielferes@gmail.com>
2025-09-22 22:49:50 -0300

    Código do gráfico de gêneros
    * Adição do arquivo de geração do gráfico de gêneros;
    
    * Deleção de arquivo repetido de geração dos gráficos de idioma;

---
+++ T1/src/main/grafico1Genero.R
@@ -0,0 +1,27 @@
+library(UpSetR)
+
+movies <- read.csv("../resources/data.csv")
+
+# Split genres into list
+all_genres <- strsplit(movies$genre, "\\,")
+
+# Flatten into one vector
+genre_vector <- unlist(all_genres)
+
+# Get unique genres
+unique_genres <- unique(genre_vector)
+
+# Build binary matrix
+genre_matrix <- sapply(unique_genres, function(g) sapply(all_genres,
+    function(movie) as.integer(g %in% movie)))
+genre_matrix <- as.data.frame(genre_matrix)
+
+# UpSet plot (shows intersections)
+upset(genre_matrix,
+      nsets = 10,
+      nintersects = 10,
+      order.by = "freq",
+      text.scale = 2.5,
+      mb.ratio = c(0.5, 0.5),
+      main.bar.color = "skyblue",
+      sets.bar.color = "grey30")
